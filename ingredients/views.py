b4ad8cdaa6f028a760e0ee1c586b1093e366d345
reciplease <bingers@ncsu.edu>
2019-04-06 16:50:42 -0400

    Figured how to tie foreign keys

--- ingredients/views.py
+++ ingredients/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import redirect
-from .models import UserIngredient
+from .models import Ingredient, UserIngredient
 from .forms import UserIngredientForm
 
 def ingredient_list(request):
-    user_ingredients = UserIngredient.objects.filter(user=request.user) # Get all ingredients for this user
+    user_ingredients = UserIngredient.objects.filter(user=request.user).select_related('ingredient') # Get all ingredients for this user
     return render(request, 'ingredients/user_ingredient_list.html', {'user_ingredients': user_ingredients})
 
 def add_user_ingredient(request):
