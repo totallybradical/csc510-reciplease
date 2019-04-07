e89199da9e9435a289a706b7e6f9203012abfe40
reciplease <bingers@ncsu.edu>
2019-04-07 11:27:14 -0400

    Added ingredient type addition screen

--- ingredients/urls.py
+++ ingredients/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ingredient_list, name='user_ingredient_list'),
-    path('ingredients/add/', views.add_user_ingredient, name='add_user_ingredient'),
+    path('add/', views.add_user_ingredient, name='add_user_ingredient'),
+    path('new/', views.add_ingredient_type, name='add_ingredient_type'),
 ]
--- ingredients/views.py
+++ ingredients/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import redirect
 from .models import Ingredient, UserIngredient
-from .forms import UserIngredientForm
+from .forms import IngredientForm, UserIngredientForm
 
 def ingredient_list(request):
     user_ingredients = UserIngredient.objects.filter(user=request.user).select_related('ingredient') # Get all ingredients for this user
@@ -20,3 +20,14 @@ def add_user_ingredient(request):
     else:
         form = UserIngredientForm()
     return render(request, 'ingredients/user_ingredient_form.html', {'form': form})
+
+def add_ingredient_type(request):
+    if request.method == "POST":
+        form = IngredientForm(request.POST)
+        if form.is_valid():
+            ingredient = form.save(commit=False)
+            ingredient.save()
+            return redirect('add_user_ingredient')
+    else:
+        form = IngredientForm()
+    return render(request, 'ingredients/ingredient_form.html', {'form': form})
