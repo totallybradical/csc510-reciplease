380dfe2d14b515b9d7044cd4cceb6c90a653403e
Craig Cravath <craigcravath@gmail.com>
2019-04-11 23:19:40 -0400

    merge changes from recommender branch

--- recommender/views.py
+++ recommender/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import redirect
+
+def recommender(request):
+    return render(request, 'recommender/search.html')
