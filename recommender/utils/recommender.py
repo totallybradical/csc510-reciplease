f132773436fb07490d4d353d68bb74e8a7328408
Andrew Keesler <ankeesler1@gmail.com>
2019-04-21 10:21:25 -0400

    I'm feeling lucky doesn't crash anymore.
    It is super ugly. Next step, filter by category.

--- recommender/utils/recommender.py
+++ recommender/utils/recommender.py
@@ -7,24 +7,29 @@ def recommend_recipe(user_ingredient_list, recipe_list, ask_a_neighbor=False, fe
         if ingredients_available(recipe, user_ingredient_list, ask_a_neighbor):
             recommended_recipes.append(recipe)
     if feeling_lucky:
-        return random.choice(recommended_recipes)
+        return [random.choice(recommended_recipes)]
     return recommended_recipes
 
 
 def ingredients_available(recipe, ingredient_list, ask_a_neighbor):
     early_exit_threshold = 1 if ask_a_neighbor else 0
     unavailable_ingredient = 0
-    for ingredient in recipe.ingredients:
-        if compare_ingredients(ingredient, ingredient_list):
-            unavailable_ingredient += 1
-        if unavailable_ingredient > early_exit_threshold:
-            return False
+
+    ingredients = recipe.ingredients.all()
+    for ingredient in ingredients:
+        recipe_ingredients = ingredient.recipeingredient_set.all()
+        for recipe_ingredient in recipe_ingredients:
+            if recipe_ingredient.id == recipe.id:
+                if compare_ingredients(recipe_ingredient, ingredient_list):
+                    unavailable_ingredient += 1
+                if unavailable_ingredient > early_exit_threshold:
+                    return False
     return True
 
 
 def compare_ingredients(recipe_ingredient, ingredient_list):
     for user_ingredient in ingredient_list:
-        if recipe_ingredient.ingredient.id == user_ingredient.ingredient.id:
+        if recipe_ingredient.id == user_ingredient.id:
             if recipe_ingredient.quantity <= user_ingredient.quantity:
                 return False
     return True
--- recommender/views.py
+++ recommender/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import SearchForm
 from recipes.models import Recipe
+from ingredients.models import UserIngredient
 from .utils.recommender import recommend_recipe
 
 def recommend(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
+            search_model = form.save(commit=False)
             if 'recommend' in request.POST:
-                search_model = form.save(commit=False)
-                print(search_model)
                 recipes = Recipe.objects.filter(category=search_model.mealCategory)
             elif 'lucky' in request.POST:
-                #recipes = recommend_recipe([], Recipe.objects., feeling_lucky=True)
-                # TODO: how can we make this utility function work :(
-                recipes = Recipe.objects.all()
-                return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+                recipes = list(Recipe.objects.all())
+                ingredients = list(UserIngredient.objects.all())
+                recommended_recipes = recommend_recipe(ingredients, recipes, feeling_lucky=True)
+            return render(request, 'recipes/recipe_list.html', {'recipes': recommended_recipes})
     else:
         form = SearchForm()
     return render(request, 'recommender/search.html', {'form': form})
