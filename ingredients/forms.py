31a688c40e602d70727278f60b636ef9d5a7a46e
reciplease <bingers@ncsu.edu>
2019-04-20 20:44:43 -0400

    Added units to the edit user ingredients form

--- ingredients/forms.py
+++ ingredients/forms.py
@@ -19,3 +19,9 @@ class EditUserIngredientForm(forms.ModelForm):
     class Meta:
         model = UserIngredient
         fields = ('quantity', 'exp_date')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        ingredient_id = self.instance.ingredient.id
+        ingredient = Ingredient.objects.get(id=ingredient_id)
+        self.fields['quantity'].label = 'Quantity (' + ingredient.quantity_units + ')'
