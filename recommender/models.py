3a54f4dedab4d1a7867e32400b48aa279c28e04d
Tyler Irons <tjirons@ncsu.edu>
2019-04-21 17:25:28 -0400

    created the required files to populate the selection list of ingredients. It still doesn't want to update, but the list is available at /ajax/load-ingredients

--- recommender/models.py
+++ recommender/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from recipes.models import RecipeCategory
+from ingredients.models import UserIngredient
+from django.conf import settings
 
 
 # Model for a Search for recipes
 class SearchModel(models.Model):
-    MEAL_CATEGORY= (
-        ('BRFST','Breakfast'),
-        ('LNCH','Lunch'),
-        ('DNR','Dinner'),
-    )
 
+    current_user = settings.AUTH_USER_MODEL
+    print(current_user)
     #ingredients = models.ForeignKey('Ingredient', on_delete=models.SET_NULL, null=True)
     #mealCategory =  models.CharField(max_length=5, choices=MEAL_CATEGORY)
     mealCategory = models.ForeignKey('recipes.RecipeCategory', on_delete=models.SET_NULL, null=True)
     askANeighbor = models.BooleanField()
+    #ingredients = models.ForeignKey(UserIngredient, on_delete=models.SET_NULL, null=True, limit_choices_to={'user': views.ingredient_list})
+    ingredients = models.ForeignKey(UserIngredient, on_delete=models.SET_NULL, null=True, )
--- recommender/urls.py
+++ recommender/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.recommend, name='search'),
+    
+    path('ajax/load-ingredients', views.load_user_ingredients, name='ajax_load_ingredients'),
 ]
